c7c9bfbbb1901eff79d329c4a8c80f43df497d7a
Nathan <nathan.arago@gmail.com>
2017-11-29 11:45:51 +0100

    réglage merge

--- web/bundles/front/scripts/secondaryMenu.js
+++ web/bundles/front/scripts/secondaryMenu.js
@@ -7,10 +7,10 @@ $(function(){
   $('#side_nav_displayer button').click(function(){
     if(!isDisplayed){
       isDisplayed = true;
-      $("#side_nav").animate({width: '100%'}, '500');
+      $("#side_nav").stop(true,true).animate({width: '100%'}, '500');
     }else{
       isDisplayed = false;
-      $("#side_nav").animate({width: '0px'}, '500');
+      $("#side_nav").stop(true,true).animate({width: '0px'}, '500');
     }
   });
 
