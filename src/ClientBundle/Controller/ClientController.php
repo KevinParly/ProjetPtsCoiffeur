e8b1280ad15242c43ac7363365acb995a954dd41
Kyusal <kevin@localhost.localdomain>
2017-12-28 16:47:14 +0100

    Ajout de l'insertion du client en BDD

--- src/ClientBundle/Controller/ClientController.php
+++ src/ClientBundle/Controller/ClientController.php
@@ -10,31 +10,47 @@ namespace ClientBundle\Controller;
 use ClientBundle\Entity\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ClientController extends Controller
 {
     /**
      * @Route("/ajoutclient",name="ajoutclient")
      */
-    public function AjoutClientAction(){
+    public function AjoutClientAction(Request $request){
         $client = new Client();
         $form = $this->createFormBuilder($client)
-            ->add('civilite',RadioType::class)
+            ->add('civilite',ChoiceType::class,array(
+                    'choices'=>array('M'=>'M','Mme'=>'Mme','Enfant'=>'Enfant'),
+                'expanded'=>true
+            ))
             ->add('nom',TextType::class)
             ->add('prenom',TextType::class)
             ->add('adresse',TextType::class)
             ->add('codePostal',TextType::class)
             ->add('ville',TextType::class)
-            ->add('dateNaissance',DateType::class)
+            ->add('dateNaissance',BirthdayType::class)
             ->add('telephone',TextType::class)
-            ->add('remise',TextType::class)
+            ->add('remise',NumberType::class)
             ->add('couleur',CheckboxType::class)
+            ->add('Enregistrer', SubmitType::class)
             ->getForm()
         ;
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $clientInsert = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($clientInsert);
+            $em->flush();
+        }
         return $this->render('ClientBundle:Default:ajoutClient.html.twig', array('form'=>$form->createView()));
     }
 }
